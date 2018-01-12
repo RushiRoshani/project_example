a735a440e631a8a1e835f9568902b55825a67d85
RushiRoshani <roshikadel@hotmail.com>
2018-01-12 23:53:20 +0545

    CalculatorImport_example

---
+++ calculator/add.py
@@ -0,0 +1,7 @@
+#Import calc module
+import calc
+#initializing the variables 
+a = int(input('Enter your first number: '))
+b = int(input('Enter your second number: '))
+#call function
+calc.add(a,b)
---
+++ calculator/calc.py
@@ -0,0 +1,17 @@
+
+#define function
+def add(a,b):
+    c=a+b
+    print("The addition of given input is",c)
+def sub(a,b):
+    c=a-b
+    print("The subtraction of given input is",c)
+def mul(a,b):
+    c=a*b
+    print("The multiplication value of given input is",c)
+def div(a,b):
+    c=a/b
+    print("The division value of given input is",c)
+
+
+
